47c27c6513b63981981fc6f9343f7e7e66a7d555
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-02-16 23:29:09 -0300

    Template pages
    Error;Loading;LoginTelegram

---
+++ src/renderer/components/Loading.vue
@@ -0,0 +1,3 @@
+<template lang="pug">
+    h1 THIS IS THE LOADING PAGE
+</template>
---
+++ src/renderer/components/LoginTelegram.vue
@@ -0,0 +1,16 @@
+<template lang="pug">
+    h1 LOGIN HERE
+      el-input-number(v-model='phone' placeholder='+55 99 999999999')
+</template>
+
+<script>
+export default {
+  data: () => ({
+    phone: ''
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
+
