e264bf86ac3ef9157bcf5efd76630132e86798f7
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-02-17 22:34:36 -0300

    Telegram login page behavior

--- src/renderer/components/LoginTelegram.vue
+++ src/renderer/components/LoginTelegram.vue
@@ -1,13 +1,41 @@
 <template lang="pug">
-    h1 LOGIN HERE
-      el-input-number(v-model='phone' placeholder='+55 99 999999999')
+    h1 Conta do telegram
+      el-input(v-model="phone" :disabled="!codeEnable" v-mask="['+## (##) ####-####', '+## (##) #####-####']" placeholder="+55 99 999999999")
+      transition(name="fade" mode="out-in")
+        el-button(@click="login" :disabled="phone.length < 8" v-if="codeEnable") Login
+      el-input(v-model="code")
+      // :disabled="codeEnable" placeholder='123456')
 </template>
 
 <script>
+
+import { ipcRenderer } from 'electron' // eslint-disable-line
+
+import { STATUS } from '../../main/telegram'
+
 export default {
-  data: () => ({
-    phone: ''
-  })
+  data() {
+    return {
+      phone: '',
+      code: ''
+    }
+  },
+  computed: {
+    codeEnable() {
+      return this.$store.state.Telegram.state !== STATUS.WAITING_CODE
+    },
+  },
+  methods: {
+    login() {
+      this.$store.dispatch('Telegram/registerPhone', { phone: this.phone })
+    },
+    sendCode() {
+      this.$store.dispatch('Telegram/saveCode', { code: this.code })
+    }
+  },
+  mounted() {
+    this.$data.phone = this.$store.state.Telegram.phone || ''
+  }
 }
 </script>
 
--- src/renderer/main.js
+++ src/renderer/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 
+import VuetheMask from 'vue-the-mask'
+
 import App from './App'
 import router, { setupRouterManager } from './router'
 import store from './store'
@@ -12,6 +14,7 @@ if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
 Vue.config.productionTip = false
 
 Vue.use(Element)
+Vue.use(VuetheMask)
 router.replace({ name: 'loading' })
 
 /* eslint-disable no-new */
