f629a860129320fd9d292fc4dfc81fb6d79ab763
Vinicius Rangel <vinicius.l.rangel@gmail.com>
2019-02-28 17:16:14 -0300

    User list fixes

--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,28 +1,27 @@
 <template lang="pug">
   div
     h1 HOMEEE
+    el-row(v-if="errList.length > 0")
+      h4 Não foi possivel enviar a ultima mensagem para:
+      .list-item(v-for="name in errList" :key="name")
+      | {{ name }}
     el-row(type="flex" justify="space-between")
       el-col(:span='10')
         h1.left Telegram
         el-card(v-for="phone in phoneList" :key="phone.id" shadow="hover")
-          el-switch(:active-text="phone.title" @input="(value) => leftInput(value, phone)" :value="using[phone.id]")
+          el-switch(:active-text="phone.title" @input="(value) => leftInput(value, phone)" :value="activeList.findIndex(e => e.id == phone.id) !== -1")
       el-col(:span='10')
         h1.right Whatsapp
         div
           el-card(v-for="phone in wppPhoneList" :key="phone" shadow="hover")
-            el-switch(:active-text="phone" @input="(value) => rightInput(value, phone)" :value="wppUsing[phone]")
+            el-switch(:active-text="phone" @input="(value) => rightInput(value, phone)" :value="wppActiveList.indexOf(phone) !== -1")
 </template>
 <script>
 
 import { ipcRenderer } from 'electron' // eslint-disable-line
+import { mapState } from 'vuex'
 
 export default {
-  data() {
-    return {
-      using: {},
-      wppUsing: {},
-    }
-  },
   computed: {
     phoneList() {
       const { phoneList, activeList } = this.$store.state.Telegram
@@ -40,30 +39,31 @@ export default {
           return 0
         })
     },
-    wppPhoneList() {
-      return this.$store.state.Whatsapp.phoneList
-    }
+    ...mapState('Telegram', ['activeList']),
+    ...mapState('Whatsapp', {
+      wppActiveList: 'activeList',
+      wppPhoneList: 'phoneList',
+      errList: 'errList'
+    })
   },
   methods: {
     leftInput(value, phone) {
-      const { id } = phone
+      let using = this.$store.state.Telegram.activeList.slice(0)
       if (value) {
-        this.$data.using[id] = true
+        using.push(phone)
       } else {
-        delete this.$data.using[phone.id]
+        using = using.filter(e => e.id !== phone.id)
       }
-      this.$data.using = Object.assign({}, this.$data.using)
-      const { phoneList } = this
-      this.$store.dispatch('Telegram/setActivePhones', phoneList.filter(e => this.$data.using[e.id]))
+      this.$store.dispatch('Telegram/setActivePhones', using)
     },
     rightInput(value, phone) {
+      let using = this.wppActiveList.slice(0)
       if (value) {
-        this.$data.wppUsing[phone] = true
+        using.push(phone)
       } else {
-        delete this.$data.wppUsing[phone]
+        using = using.filter(e => e !== phone)
       }
-      this.$data.wppUsing = Object.assign({}, this.$data.wppUsing)
-      this.$store.dispatch('Whatsapp/setActive', this.$data.wppUsing)
+      this.$store.dispatch('Whatsapp/setActive', using)
     },
     refreshWpp() {
       ipcRenderer.send('Whatsapp/LIST_USERS')
